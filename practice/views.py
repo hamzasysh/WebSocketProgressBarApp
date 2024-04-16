40f575dfca31ba903a8d8e8914ebd11af1a2a9c1
Muhammad Hamza <hamzaashraf@systemheuristics.com>
2024-04-16 12:34:27 +0500

    practice view file changed

--- practice/views.py
+++ practice/views.py
@@ -9,3 +9,27 @@ def show_page(request):
 def taskDispatcher(request):
     task=my_task.delay(10)  # Start the task asynchronously
     return JsonResponse({'task_id': task.id},status=200)
+
+def bubble_sort(arr):
+    """
+    Sorts a list of integers using the Bubble Sort algorithm.
+    
+    Args:
+    - arr (list of int): List of integers to be sorted.
+    
+    Returns:
+    - list of int: Sorted list of integers.
+    """
+    n = len(arr)
+    
+    # Traverse through all elements in the list
+    for i in range(n):
+        
+        # Last i elements are already in place, so we don't need to check them
+        for j in range(0, n-i-1):
+            
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    
+    return arr
